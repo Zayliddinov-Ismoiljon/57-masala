59f4ba7daafb082ffd00f2d9e3ccfd4adf33acce
Zayliddinov-Ismoiljon <zayliddinovismoiljon@gmail.com>
2022-03-23 17:57:09 +0500

    Innitial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// Masala: n soni berilgan dastlabki  nta elementdan iborat massiv hozil qiling va elemntlarini chiqaring
+
+let n=5;
+const a=[];
+
+for(i=0; i<=n; i++){
+    console.log(a.push(i));
+}
